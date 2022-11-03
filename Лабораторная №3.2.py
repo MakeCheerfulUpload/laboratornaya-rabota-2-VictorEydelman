c2f0c7f272e59f9affcd2d7da4b721a20b7dc383
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2022-11-03 16:22:40 +0300

    Delete Лабораторная №3.2.py

--- Лабораторная №3.2.py
+++
@@ -1,7 +0,0 @@
-import re
-n=re.split(r' ',str(input()))
-for i in range(len(n)):
-    d=re.split(r'[А,И,О,У,Ы,Э,Е,Ё,Ю,Я]',n[i].upper())
-    g=re.split(r'[А,И,О,У,Ы,Э,Е,Ё,Ю,Я]{2}',n[i].upper())
-    if sum(map(len,d))<=3 and len(g)>1:
-        print(n[i])
